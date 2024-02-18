d54a2bc903d17583ffb014b8fbc1b93c638f6fff
akhil-iitkgp <akhilaashiyan17@gmail.com>
2024-02-18 16:14:41 +0530

    Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar=()=>{
     return(
         <div>
             <nav className="bg-orange-600 py-4">
-                <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
+                <h1 className="text-3xl font-bold text-center text-white">Welcome to CourseHub</h1>
             </nav>
         </div>
     )
