d8e87110742d6f8a5f1af489bb4366f07b5a7b9f
akhil-iitkgp <akhilaashiyan17@gmail.com>
2024-02-18 15:26:16 +0530

    all functions added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar"
 import Filter from "./components/Filter"
 import Spinner from "./components/Spinner"
 import Cards from "./components/Cards"
-import { filterData } from "./data";
+import { filterData,apiUrl } from "./data";
+import { toast } from "react-toastify";
 
 const App = () => {
   const[category, setCategory]=useState(filterData[0].title);
   const[loading,setloading]=useState(true);
-  return <div>
+  const[courses,setCourses]=useState(null);
+  async function fetchData(){
+    setloading(true);
+    try{
+      let response=await fetch(apiUrl);
+      let output = await response.json();
+      setCourses(output.data);
+    }
+    catch(error){
+      toast.error("Problem in the network");
+    }
+    setloading(false);
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+
+  return (
+    <div>
     <div>
       <Navbar/>
     </div>
@@ -21,10 +42,11 @@ const App = () => {
 
     <div>
       {
-        loading?(<Spinner/>) : (<Cards></Cards>) 
+        loading?(<Spinner/>) : (<Cards courses={courses} category={category}></Cards>) 
       }
     </div>
-  </div>;
+  </div>
+  );
 };
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,4 +2,61 @@ import React from 'react'
 import {FcLike,FcLikePlaceholder} from "react-icons/fc"
 import { toast } from 'react-toastify';
 
-const 
+const Card =(props)=>{
+    let course = props.course;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses= props.setLikedCourses;
+
+    function clickHandler(){
+        if(likedCourses.includes(course.id)) {
+            //pehle se like hua pada tha
+            setLikedCourses( (prev) => prev.filter((cid)=> (cid !== course.id) )  );// prev is previous state ki id hai 
+            toast.warning("like removed");
+        }
+        else {
+            //pehle se like nahi hai ye course
+            //insert karna h ye course liked courses me 
+            if(likedCourses.length === 0 ) {
+                setLikedCourses([course.id]);
+            }
+            else {
+                //non-empty pehle se
+                setLikedCourses((prev) => [...prev, course.id]);
+            }
+            toast.success("Liked Successfully");
+        }
+    }
+
+    return (
+        <div className='w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden'>
+            <div>
+            <img src={course.image.url} alt='kkk'/>
+            <div>
+                <button onClick={clickHandler}>
+                    {
+                        likedCourses.includes(course.id) ? 
+                        (<FcLike fontSize="1.75rem" />)
+                        :(<FcLikePlaceholder fontSize="1.75rem" />)
+                    }
+
+                </button>
+            </div>
+            </div>
+
+
+            <div className='p-4'>
+            <p className="text-white font-semibold text-lg leading-6">{course.title}</p>
+            <p className='mt-2 text-white'>
+                    {
+                        course.description.length >100 ? 
+                        (course.description.substr(0,100)) + "..." :
+                        (course.description)
+                    }
+            </p>
+        </div>
+
+        </div>
+    )
+
+}
+export default Card
