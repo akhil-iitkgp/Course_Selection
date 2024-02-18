4e5d14cebb6071038e60bb4b45bc918c31f2b434
akhil-iitkgp <akhilaashiyan17@gmail.com>
2024-02-18 14:10:16 +0530

    Nav bar, Filter added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar"
+import Filter from "./components/Filter"
+import Spinner from "./components/Spinner"
+import Cards from "./components/Cards"
+import { filterData } from "./data";
 
 const App = () => {
-  return <div>App</div>;
+  const[category, setCategory]=useState(filterData[0].title);
+  const[loading,setloading]=useState(true);
+  return <div>
+    <div>
+      <Navbar/>
+    </div>
+    <div>
+      <Filter
+      filterData={filterData}
+      category={category}
+      setCategory={setCategory}/>
+    </div>
+
+    <div>
+      {
+        loading?(<Spinner/>) : (<Cards></Cards>) 
+      }
+    </div>
+  </div>;
 };
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {FcLike,FcLikePlaceholder} from "react-icons/fc"
+import { toast } from 'react-toastify';
+
+const 
